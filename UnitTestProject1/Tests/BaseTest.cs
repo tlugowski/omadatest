07631032d30b7c660dda6fed8cc2a19b41d701d3
Tomasz Ługowski <t.lugowski@wp.pl>
2018-07-30 18:13:56 +0200

    second not important commit

---
+++ UnitTestProject1/Tests/BaseTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        protected IWebDriver Driver { get; set; }
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [SetUp]
+        public void Initalize()
+        {
+            string browser = "Chrome";
+            switch(browser)
+            {
+                case "Chrome":
+                    Driver = new ChromeDriver();
+                    break;
+                case "FireFox":
+                    Driver = new FirefoxDriver();
+                    break;
+            }
+            Driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void CloseTest()
+        {
+            String mainWindow = Driver.CurrentWindowHandle;
+            Driver.WindowHandles.Where(w => w != mainWindow).ToList()
+           .ForEach(w =>{Driver.SwitchTo().Window(w);
+            Driver.Close();});
+            Driver.SwitchTo().Window(mainWindow).Close();
+        }
+    }
+}
---
+++ UnitTestProject1/Tests/OmadaTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class OmadaTests : BaseTest
+    {
+
+
+        [TestCategory("FireFox")]
+        [Test]
+        public void OmadaPageTest()
+        {
+            var test = new FirstPage(Driver);
+            test.GoToPage();
+            test.ExecuteSearch();
+            test.GoToLinkAndVerifyResult();
+            test.NavigateToNewsAndVerifyActualNews();
+            test.GoToHomePageAndChangeLocation();
+            test.OpenPageInNewTab();
+            test.MakeSurePrivacyPolicyNotAppearAgain();
+            test.FillDataAndDownloadPDF();
+        }
+
+
+
+
+
+
+
+    }
+}
